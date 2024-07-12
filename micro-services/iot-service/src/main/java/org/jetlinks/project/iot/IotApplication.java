61cec4be5b3b0457e5ac379fde6760749eb2514d
gyl <1554863190@qq.com>
2024-07-12 14:46:00 +0800

    build: update project

---
+++ micro-services/iot-service/src/main/java/org/jetlinks/project/iot/IotApplication.java
@@ -0,0 +1,35 @@
+package org.jetlinks.cloud;
+
+import org.hswebframework.web.authorization.basic.configuration.EnableAopAuthorize;
+import org.hswebframework.web.crud.annotation.EnableEasyormRepository;
+import org.hswebframework.web.logging.aop.EnableAccessLogger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication(
+    scanBasePackages = {"org.jetlinks.pro","org.jetlinks.project"},
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        ElasticsearchRestClientAutoConfiguration.class,
+        RabbitAutoConfiguration.class,
+        KafkaAutoConfiguration.class
+    })
+@EnableCaching
+@EnableEasyormRepository({
+    "org.jetlinks.pro.**.entity",
+    "org.jetlinks.project.**.entity"
+})
+@EnableAopAuthorize
+@EnableAccessLogger
+public class IotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotApplication.class, args);
+    }
+
+}
